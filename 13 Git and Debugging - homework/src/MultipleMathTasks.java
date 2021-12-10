8a7890037064bc9e5335660ca85ef70f7f6538fd
tsenovi <vandertsen@gmail.com>
2021-12-10 02:48:02 +0200

    Updated 3rd task from homework, rm bad code.

--- 13 Git and Debugging - homework/src/MultipleMathTasks.java
+++ 13 Git and Debugging - homework/src/MultipleMathTasks.java
@@ -3,107 +3,81 @@ import java.util.Scanner;
 public class MultipleMathTasks {
 
     public static void main(String[] args) {
-        printProgramName();
         printProgramFeatures();
-        int task = isValidTask(getNumber());
+        int task = Integer.parseInt(getNumber());
         executeTask(task);
-    }
-
-    public static void executeTask(int task) {
-        switch (task) {
-            case 1 -> {
-                System.out.println("Task 1: ");
-                int number = (int) isPositive(getNumber());
-                System.out.println("Result: " + swapNumbersOfTwoDigitNumber(number));
-            }
-            case 2 -> {
-                System.out.println("Task 2: ");
-                double numberOne = isPositive(getNumber());
-                double numberTwo = isPositive(getNumber());
-                System.out.println("Result: " + calculateAverageOfTwoNumbers(numberOne, numberTwo));
-            }
-            case 3 -> {
-                System.out.println("Task 3: ");
-                double numberA = isDifferentWithZero(getNumber());
-                double numberB = getNumber();
-                System.out.println("Result: x = " + calculateLinearEquation(numberA, numberB));
-            }
-            default -> printErrorMessage();
-        }
-    }
-
-    public static double isPositive(double number) {
-        Scanner scanner = new Scanner(System.in);
-        while (number <= 9) {
-            System.out.println("Insert positive number: ");
-            while (!scanner.hasNextInt()) {
-                printErrorMessage();
-                scanner.next();
-            }
-            number = scanner.nextInt();
-        }
-        return number;
-    }
-
-    public static double isDifferentWithZero(double number) {
-        Scanner scanner = new Scanner(System.in);
-        while (number == 0) {
-            System.out.println("Insert number different than 0: ");
-            while (!scanner.hasNextInt()) {
-                printErrorMessage();
-                scanner.next();
-            }
-            number = scanner.nextInt();
-        }
-        return number;
-    }
-
-    public static int isValidTask(int task) {
-        Scanner scanner = new Scanner(System.in);
-        while (task < 1 || task > 3) {
-            printErrorMessage();
-            while (!scanner.hasNextInt()) {
-                printErrorMessage();
-                scanner.next();
-            }
-            task = scanner.nextInt();
-        }
-        return task;
-    }
-
-    public static double calculateLinearEquation(double numberOne, double numberTwo) {
-        return (-1) * (numberTwo / numberOne);
-    }
-
-    public static double calculateAverageOfTwoNumbers(double numberOne, double numberTwo) {
-        return (numberOne + numberTwo) / 2;
-    }
-
-    public static int swapNumbersOfTwoDigitNumber(int number) {
-        int firstDigit = number % 10;
-        int secondDigit = number / 10;
-        return Integer.parseInt("" + firstDigit + secondDigit);
-    }
-
-    public static int getNumber() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Insert number: ");
-        return scanner.nextInt();
-    }
-
-    public static void printErrorMessage() {
-        System.out.println("Invalid input");
     }
 
     public static void printProgramFeatures() {
         System.out.println("1. Switch the digits of a two digit number.");
         System.out.println("2. Average calculation of two numbers.");
         System.out.println("3. Calculation of linear equation a * x + b = 0");
+        System.out.print("Choose Task: ");
     }
 
-    public static void printProgramName() {
-        System.out.println("--------- Program Tasks ---------");
+    public static void printErrorMessage() {
+        System.out.println("Invalid input");
     }
 
+    public static String getNumber() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
 
+    public static void executeTask(int task) {
+        switch (task) {
+            case 1 -> {
+                System.out.print("Insert two digit number: ");
+                int number = Integer.parseInt(getNumber());
+                isTwoDigitsPositiveNumber(number);
+            }
+            case 2 -> {
+                System.out.print("Insert first number: ");
+                double numberOne = Double.parseDouble(getNumber());
+                System.out.print("Insert second number: ");
+                double numberTwo = Double.parseDouble(getNumber());
+                isBothNumbersPositive(numberOne, numberTwo);
+            }
+            case 3 -> {
+                System.out.print("Insert number \"a\": ");
+                double numberA = Double.parseDouble(getNumber());
+                System.out.print("Insert number \"b\": ");
+                double numberB = Double.parseDouble(getNumber());
+                isNumberADifferentWithZero(numberA, numberB);
+            }
+            default -> printErrorMessage();
+        }
+    }
+
+    private static void isTwoDigitsPositiveNumber(int number) {
+        if (number < 10 || number > 99) printErrorMessage();
+        else
+            System.out.println("Result: " + swapDigits(number));
+    }
+
+    private static void isBothNumbersPositive(double numberOne, double numberTwo) {
+        if (numberOne < 1 || numberTwo < 1) printErrorMessage();
+        else
+            System.out.println("Result: " + calculateAverage(numberOne, numberTwo));
+    }
+
+    private static void isNumberADifferentWithZero(double numberA, double numberB) {
+        if (numberA == 0) printErrorMessage();
+        else
+            System.out.println("Result: x = " + calculateLinearEquation(numberA, numberB));
+    }
+
+    public static int swapDigits(int number) {
+        int firstDigit = number / 10;
+        int secondDigit = number % 10;
+        return Integer.parseInt("" + secondDigit + firstDigit);
+    }
+
+    public static double calculateAverage(double numberOne, double numberTwo) {
+        return (numberOne + numberTwo) / 2;
+    }
+
+    public static double calculateLinearEquation(double numberOne, double numberTwo) {
+        return (-1) * (numberTwo / numberOne);
+    }
 }
