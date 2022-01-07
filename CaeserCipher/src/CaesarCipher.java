5770321d1d9a4aefca465dd4922bfcc99f826a66
tsenovi <vandertsen@gmail.com>
2022-01-07 21:39:04 +0200

    Updated variables and if-else construction, created new methods and added constant variables

--- CaeserCipher/src/CaesarCipher.java
+++ CaeserCipher/src/CaesarCipher.java
@@ -1,8 +1,10 @@
 import java.util.Arrays;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class CaesarCipher {
+
+    public static final char LETTER_SMALL_A = 'a';
+    public static final char LETTER_SMALL_Z = 'z';
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -12,40 +14,44 @@ public class CaesarCipher {
     }
 
     public static String sortText(String text) {
-        text = text.toLowerCase(Locale.ROOT);
+        text = getLowerCaseText(text);
         char[] chars = text.toCharArray();
         Arrays.sort(chars);
         return new String(chars);
     }
 
-    public static char findMostFrequentChar(String chars) {
-        char maxChar = chars.charAt(0);
+    public static char findMostFrequentChar(String text) {
+        char frequentChar = text.charAt(0);
         char currentChar;
-        char previousChar = chars.charAt(0);
-        int maxCharCounter = 0;
+        char previousChar = text.charAt(0);
+        int frequentCharCounter = 0;
         int currentCharCounter = 0;
-        for (int i = 0; i < chars.length(); i++) {
-            currentChar = chars.charAt(i);
-            if (currentChar < 97 || currentChar > 122) {
-                i++;
-            } else {
+        for (int i = 0; i < text.length(); i++) {
+            currentChar = text.charAt(i);
+            if (isSmallLetter(currentChar)) {
                 if (currentChar != previousChar) {
                     currentCharCounter = 0;
-                }
-
-                if (chars.charAt(i) == currentChar) {
+                } else {
                     currentCharCounter++;
                 }
 
-                if (currentCharCounter > maxCharCounter) {
-                    maxCharCounter = currentCharCounter;
-                    maxChar = currentChar;
+                if (currentCharCounter > frequentCharCounter) {
+                    frequentCharCounter = currentCharCounter;
+                    frequentChar = currentChar;
                 }
 
                 previousChar = currentChar;
             }
         }
 
-        return maxChar;
+        return frequentChar;
+    }
+
+    public static boolean isSmallLetter(char symbol) {
+        return symbol >= LETTER_SMALL_A && symbol <= LETTER_SMALL_Z;
+    }
+
+    public static String getLowerCaseText(String text) {
+        return text.toLowerCase();
     }
 }
