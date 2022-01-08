a7714d287c2f488b460bfbbd0c9ca0845438d3dd
tsenovi <vandertsen@gmail.com>
2022-01-08 17:09:18 +0200

    Updated variables, created method calculateOffset

--- CaeserCipher/src/CaesarCipher.java
+++ CaeserCipher/src/CaesarCipher.java
@@ -3,52 +3,67 @@ import java.util.Scanner;
 
 public class CaesarCipher {
 
-    public static final char LETTER_SMALL_A = 'a';
-    public static final char LETTER_SMALL_Z = 'z';
+    public static final int NUMBER_OF_ALPHABET_LETTERS = 26;
+    public static final char SMALL_LETTER_A = 'a';
+    public static final char SMALL_LETTER_Z = 'z';
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String encryptedText = scanner.nextLine();
         String sortedText = sortText(encryptedText);
-        char frequentSymbol = findMostFrequentChar(sortedText);
+        char frequentLetter = findMostFrequentLetter(sortedText);
+
+        char[] mostCommonLetters = {'e', 't', 'h'};
+        printResults(mostCommonLetters, frequentLetter);
+    }
+
+    public static void printResults(char[] letters, char frequentLetter) {
+        for (int index = 0; index < letters.length; index++) {
+            int complementaryOffset = calculateOffset(letters[index], frequentLetter);
+        }
     }
 
     public static String sortText(String text) {
         text = getLowerCaseText(text);
-        char[] chars = text.toCharArray();
-        Arrays.sort(chars);
-        return new String(chars);
+        char[] charsArray = text.toCharArray();
+        Arrays.sort(charsArray);
+        return new String(charsArray);
     }
 
-    public static char findMostFrequentChar(String text) {
-        char frequentChar = text.charAt(0);
-        char currentChar;
-        char previousChar = text.charAt(0);
-        int frequentCharCounter = 0;
-        int currentCharCounter = 0;
+    public static int calculateOffset(char currentLetter, char frequentLetter) {
+        int offset = Math.abs(currentLetter - frequentLetter);
+        return NUMBER_OF_ALPHABET_LETTERS - offset;
+    }
+
+    public static char findMostFrequentLetter(String text) {
+        char frequentLetter = text.charAt(0);
+        char currentLetter;
+        char previousLetter = text.charAt(0);
+        int frequentLetterCounter = 0;
+        int currentLetterCounter = 0;
         for (int i = 0; i < text.length(); i++) {
-            currentChar = text.charAt(i);
-            if (isSmallLetter(currentChar)) {
-                if (currentChar != previousChar) {
-                    currentCharCounter = 0;
+            currentLetter = text.charAt(i);
+            if (isSmallLetter(currentLetter)) {
+                if (currentLetter != previousLetter) {
+                    currentLetterCounter = 0;
                 } else {
-                    currentCharCounter++;
+                    currentLetterCounter++;
                 }
 
-                if (currentCharCounter > frequentCharCounter) {
-                    frequentCharCounter = currentCharCounter;
-                    frequentChar = currentChar;
+                if (currentLetterCounter > frequentLetterCounter) {
+                    frequentLetterCounter = currentLetterCounter;
+                    frequentLetter = currentLetter;
                 }
 
-                previousChar = currentChar;
+                previousLetter = currentLetter;
             }
         }
 
-        return frequentChar;
+        return frequentLetter;
     }
 
-    public static boolean isSmallLetter(char symbol) {
-        return symbol >= LETTER_SMALL_A && symbol <= LETTER_SMALL_Z;
+    public static boolean isSmallLetter(char letter) {
+        return letter >= SMALL_LETTER_A && letter <= SMALL_LETTER_Z;
     }
 
     public static String getLowerCaseText(String text) {
